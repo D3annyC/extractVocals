6a871d01de5103fc1721dc26269a3e6668cf04d1
D3annyC <ch.chung127@gmail.com>
2019-04-03 12:15:53 +0800

    upload first edit

---
+++ plot_vocal_separation.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
+
+import librosa.display
+
+# Load an example with vocals.
+y, sr = librosa.load('audio/Jay Chou_Far_Away', duration=120)
+
+
+# And compute the spectrogram magnitude and phase
+S_full, phase = librosa.magphase(librosa.stft(y))
+
+
+# Plot a 5-second slice of the spectrum
+idx = slice(*librosa.time_to_frames([30, 35], sr=sr))
+plt.figure(figsize=(12, 4))
+librosa.display.specshow(librosa.amplitude_to_db(S_full[:, idx], ref=np.max),
+                         y_axis='log', x_axis='time', sr=sr)
+plt.colorbar()
+plt.tight_layout()
+
+S_filter = librosa.decompose.nn_filter(S_full,
+                                       aggregate=np.median,
+                                       metric='cosine',
+                                       width=int(librosa.time_to_frames(2, sr=sr)))
+
+
+S_filter = np.minimum(S_full, S_filter)
+
+margin_i, margin_v = 2, 10
+power = 2
+
+mask_i = librosa.util.softmask(S_filter,
+                               margin_i * (S_full - S_filter),
+                               power=power)
+
+mask_v = librosa.util.softmask(S_full - S_filter,
+                               margin_v * S_filter,
+                               power=power)
+
+
+S_foreground = mask_v * S_full
+S_background = mask_i * S_full
+
+
+# sphinx_gallery_thumbnail_number = 2
+
+plt.figure(figsize=(12, 8))
+plt.subplot(3, 1, 1)
+librosa.display.specshow(librosa.amplitude_to_db(S_full[:, idx], ref=np.max),
+                         y_axis='log', sr=sr)
+plt.title('Full spectrum')
+plt.colorbar()
+
+plt.subplot(3, 1, 2)
+librosa.display.specshow(librosa.amplitude_to_db(S_background[:, idx], ref=np.max),
+                         y_axis='log', sr=sr)
+plt.title('Background')
+plt.colorbar()
+plt.subplot(3, 1, 3)
+librosa.display.specshow(librosa.amplitude_to_db(S_foreground[:, idx], ref=np.max),
+                         y_axis='log', x_axis='time', sr=sr)
+plt.title('Foreground')
+plt.colorbar()
+plt.tight_layout()
+plt.show()
