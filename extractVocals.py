12288db969667add25d05884c580800bff840e27
D3annyC <ch.chung127@gmail.com>
2019-04-03 14:15:44 +0800

    modified format of file name

---
+++ extractVocals.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+
+from pydub import AudioSegment
+from pydub.playback import play
+
+filePath = 'audio/Jay_Chou_Far_Away.mp3'
+
+# Load an example with vocals.
+y, sr = librosa.load(filePath, duration=120)
+
+
+# And compute the spectrogram magnitude and phase
+S_full, phase = librosa.magphase(librosa.stft(y))
+
+
+# Plot a 5-second slice of the spectrum
+idx = slice(*librosa.time_to_frames([30, 35], sr=sr))
+plt.figure(figsize=(12, 4))
+librosa.display.specshow(librosa.amplitude_to_db(S_full[:, idx], ref=np.max),
+                         y_axis='log', x_axis='time', sr=sr)
+plt.colorbar()
+plt.tight_layout()
+
+S_filter = librosa.decompose.nn_filter(S_full,
+                                       aggregate=np.median,
+                                       metric='cosine',
+                                       width=int(librosa.time_to_frames(2, sr=sr)))
+
+
+S_filter = np.minimum(S_full, S_filter)
+
+margin_i, margin_v = 2, 10
+power = 2
+
+mask_i = librosa.util.softmask(S_filter,
+                               margin_i * (S_full - S_filter),
+                               power=power)
+
+mask_v = librosa.util.softmask(S_full - S_filter,
+                               margin_v * S_filter,
+                               power=power)
+
+
+S_foreground = mask_v * S_full
+S_background = mask_i * S_full
+
+
+# sphinx_gallery_thumbnail_number = 2
+
+plt.figure(figsize=(12, 8))
+plt.subplot(3, 1, 1)
+librosa.display.specshow(librosa.amplitude_to_db(S_full[:, idx], ref=np.max),
+                         y_axis='log', sr=sr)
+plt.title('Full spectrum')
+plt.colorbar()
+
+plt.subplot(3, 1, 2)
+librosa.display.specshow(librosa.amplitude_to_db(S_background[:, idx], ref=np.max),
+                         y_axis='log', sr=sr)
+plt.title('Background')
+plt.colorbar()
+plt.subplot(3, 1, 3)
+librosa.display.specshow(librosa.amplitude_to_db(S_foreground[:, idx], ref=np.max),
+                         y_axis='log', x_axis='time', sr=sr)
+plt.title('Foreground')
+plt.colorbar()
+plt.tight_layout()
+plt.show()
+
+
+# read in audio file and get the two mono tracks
+sound_stereo = AudioSegment.from_file(
+    filePath, format="mp3")
+sound_monoL = sound_stereo.split_to_mono()[0]
+sound_monoR = sound_stereo.split_to_mono()[1]
+
+# Invert phase of the Right audio file
+sound_monoR_inv = sound_monoR.invert_phase()
+
+# Merge two L and R_inv files, this cancels out the centers
+sound_CentersOut = sound_monoL.overlay(sound_monoR_inv)
+
+# Export merged audio file
+fh = sound_CentersOut.export(
+    filePath.lstrip('.mp3') + '-extractVocals.mp3', format="mp3")
--- plot_vocal_separation.py
+++ plot_vocal_separation.py
@@ -3,11 +3,11 @@ from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
 import librosa
-
 import librosa.display
 
+
 # Load an example with vocals.
-y, sr = librosa.load('audio/Jay Chou_Far_Away', duration=120)
+y, sr = librosa.load('audio/Jay_Chou_Far_Away.mp3', duration=120)
 
 
 # And compute the spectrogram magnitude and phase
